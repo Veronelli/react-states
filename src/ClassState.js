7ec1689d5b57c89d524326ce7faf3c0387391e10
Facundo Veronelli <fveronelli@gruposimpli.com>
2023-11-30 09:13:01 -0300

    add livecycle method

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { Loading } from "./Loading";
 
 class ClassState extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       error: false,
+      loading: true
     };
+  }
+  
+  componentWillUnmount(){
+    console.log("componentWillUnmount")
+  }
+
+  componentDidMount(){
+    console.log("componentDidMount")
+  }
+  
+  componentDidUpdate(){
+    if(this.state.loading){
+      setTimeout(()=>{
+        this.setState({loading:false})
+      },1000)
+    }
   }
   render() {
     return (
@@ -13,6 +31,7 @@ class ClassState extends React.Component {
         <h2>Eliminar {this.props.name}</h2>
         <p>Por favor escribe el codigo de seguridad.</p>
         {this.state.error && <p>Error: el codigo es incorrecto</p>}
+        {!!this.state.loading && <Loading/>}
         <input placeholder="Codigo de seguridad" type="password" />
         <button
           onClick={() => {
