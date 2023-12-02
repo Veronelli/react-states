6a251699a5d4c4f42713aaff60fa7ef77be65e71
Veronelli <facu2000veronelli@outlook.com>
2023-12-02 10:49:37 -0300

    add value state in class state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { UseState } from './UseState';
 import { ClassState } from './ClassState';
--- src/ClassState.js
+++ src/ClassState.js
@@ -1,43 +1,53 @@
 import React from "react";
 import { Loading } from "./Loading";
 
+const SECURITY_KEY = "SECRET";
+
 class ClassState extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       error: false,
-      loading: true
+      loading: false,
+      value: "",
     };
   }
-  
-  componentWillUnmount(){
-    console.log("componentWillUnmount")
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
   }
 
-  componentDidMount(){
-    console.log("componentDidMount")
+  componentDidMount() {
+    console.log("componentDidMount");
   }
-  
-  componentDidUpdate(){
-    if(this.state.loading){
-      setTimeout(()=>{
-        this.setState({loading:false})
-      },1000)
+
+  componentDidUpdate() {
+    console.log(this.state)
+    if (this.state.loading) {
+      setTimeout(() => {
+        if (this.state.value === SECURITY_KEY) {
+          this.setState({...this.state, loading:false, error: false });
+        } else this.setState({ ...this.state, loading: false, error: true });
+      }, 1000);
     }
   }
   render() {
+    const { value, loading, error } = this.state;
     return (
       <div>
         <h2>Eliminar {this.props.name}</h2>
         <p>Por favor escribe el codigo de seguridad.</p>
-        {this.state.error && <p>Error: el codigo es incorrecto</p>}
-        {!!this.state.loading && <Loading/>}
-        <input placeholder="Codigo de seguridad" type="password" />
-        <button
-          onClick={() => {
-            this.setState((prev) => ({ error: !prev.error }));
+        {error && <p>Error: el codigo es incorrecto</p>}
+        {!!loading && <Loading />}
+        <input
+          value={value}
+          placeholder="Codigo de seguridad"
+          type="password"
+          onChange={(event) => {
+            this.setState({ ...this.state, value: event.target.value });
           }}
-        >
+        />
+        <button onClick={() => this.setState({ ...this.state, loading: true, })}>
           Comprobar
         </button>
       </div>
